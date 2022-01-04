3064732cbacb2114770316603fa197780dcf5ebe
ravindar86 <ravindar.mca@gmail.com>
2022-01-04 08:55:11 +0530

    Configuring DataStax Astra Database

--- src/main/java/com/message/inboxapp/InboxAppApplication.java
+++ src/main/java/com/message/inboxapp/InboxAppApplication.java
@@ -1,11 +1,14 @@
 package com.message.inboxapp;
 
+import com.message.inboxapp.configuration.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -15,9 +18,15 @@ public class InboxAppApplication {
         SpringApplication.run(InboxAppApplication.class, args);
     }
 
-    @GetMapping("/user")
+    /*@GetMapping("/user")
     public String user(@AuthenticationPrincipal OAuth2User principal){
         System.out.println(principal);
         return principal.getAttribute("name");
+    }*/
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
 }
